8e48c949e47e67cc234d7f5e8e852723575fcbe7
Emily R <emilyroulund@gmail.com>
2019-04-17 17:35:21 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -36,7 +36,7 @@ end
 
 def until_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  counter = 1 
+  counter = 0 
   until counter == number_of_times
   puts phrase 
   counter += 1
