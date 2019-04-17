f5bbb241da4a96182425ccaabb0ea76a2f85c37d
Emily R <emilyroulund@gmail.com>
2019-04-17 17:38:41 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -47,5 +47,7 @@ def for_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   counter = 1..number_of_times
   for number in counter 
+  puts phrase 
+end 
 end
 
